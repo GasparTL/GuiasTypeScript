4edcec7f379773b5904abbf7cf3363bc18b5cf41
Gaspar Toribio Lucas <153266598+GeneralRis3r@users.noreply.github.com>
2023-12-22 14:02:35 -0600

    Add files via upload
    Hola Mundo con TypeScript

---
+++ HolaMundoTypeScript/funciones.js
@@ -0,0 +1,14 @@
+"use strict";
+let saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+let numero = 10;
+let cualquiera;
+cualquiera = 10;
+cualquiera = "cadena";
+const PI = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+saludar();
---
+++ HolaMundoTypeScript/funciones.ts
@@ -0,0 +1,17 @@
+let saludo:string = "HolaMundo desde TypeScript";
+
+saludo = "cambio";
+
+let numero:number = 10;
+
+let cualquiera:any;
+cualquiera = 10;
+cualquiera = "cadena";
+
+const PI = 3.14;
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+saludar();
